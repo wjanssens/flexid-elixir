f79a7a9f7a061fd2b3178e507e9ffba43318c0f6
Warren Janssens <warren.janssens@gmail.com>
2017-03-30 09:12:25 -0600

    converted implementation to an Agent

--- lib/flexid.ex
+++ lib/flexid.ex
@@ -1,4 +1,5 @@
 use Bitwise
+require Logger
 
 defmodule FlexId do
   @moduledoc """
@@ -12,40 +13,52 @@ defmodule FlexId do
             epoch: 0,
             sequence: 0
 
-  def new() do
-    %FlexId{}
+  def start_link() do
+    Agent.start_link(fn -> %FlexId{} end)
   end
 
-  def set_sequence_bits(id, bits) when bits >=0 and bits < 15 do
-    %{id | sequence_bits: bits, sequence_mask: make_mask(bits)}
+  def set_sequence_bits(agent, bits) when bits >= 0 and bits < 15 do
+    Agent.update(agent, fn state -> %{state | sequence_bits: bits, sequence_mask: make_mask(bits)} end)
   end
 
-  def set_shard_bits(id, bits) when bits >=0 and bits < 15 do
-    %{id | shard_bits: bits, shard_mask: make_mask(bits)}
+  def set_shard_bits(agent, bits) when bits >=0 and bits < 15 do
+    Agent.update(agent, fn state -> %{state | shard_bits: bits, shard_mask: make_mask(bits)} end)
   end
 
-  def set_sequence(id, seq) do
-    %{id | sequence: seq}
+  def set_sequence(agent, seq) do
+    Agent.update(agent, fn state -> %{state | sequence: seq} end)
   end
 
-  def set_epoch(id, epoch) do
-    %{id | epoch: epoch}
+  def set_epoch(agent, epoch) do
+    Agent.update(agent, fn state -> %{state | epoch: epoch} end)
   end
 
-  def extract_raw_millis(id, value) do
-    value >>> (id.sequence_bits + id.shard_bits)
+  def extract_raw_millis(agent, value) do
+    Agent.get(agent, fn state -> value >>> (state.sequence_bits + state.shard_bits) end)
   end
 
-  def extract_millis(id, value) do
-    (value >>> (id.sequence_bits + id.shard_bits)) + id.epoch
+  def extract_millis(agent, value) do
+    Agent.get(agent, fn state -> (value >>> (state.sequence_bits + state.shard_bits)) + state.epoch end)
   end
 
-  def extract_sequence(id, value) do
-    (value >>> id.shard_bits) &&& id.sequence_mask
+  def extract_sequence(agent, value) do
+    Agent.get(agent, fn state -> (value >>> state.shard_bits) &&& state.sequence_mask end)
   end
 
-  def extract_shard(id, value) do
-      value &&& id.shard_mask
+  def extract_shard(agent, value) do
+    Agent.get(agent, fn state -> value &&& state.shard_mask end)
+  end
+
+  def log(agent) do
+    Agent.cast(agent, fn state ->
+      ms = round(:math.pow 2, (64 - state.sequence_bits - state.shard_bits - 1))
+      yr = round(ms / 1000 / 60 / 60 / 24 / 365)
+      fr = DateTime.from_unix! state.epoch, :millisecond
+      to = DateTime.from_unix! (state.epoch + ms), :millisecond
+      seq = :math.pow 2, state.sequence_bits
+      shd = :math.pow 2, state.shard_bits
+      Logger.info "Ids have a time range of #{yr} years (#{fr} to #{to}), #{seq} sequences, #{shd} shards"
+   end)
   end
 
   defp make_mask(bits) do
@@ -53,32 +66,33 @@ defmodule FlexId do
   end
 
   @doc """
-  Generate an Id Value.
+  Helper for generating a 16-bit shard value from the last two bytes of the sha1 hash of a text value
 
   ## Examples
-      iex> id = FlexId.new
-      {value, id} = FlexId.generate(id, 0xB1)
+      shard = FlexId.make_shard("username")
   """
-  def generate(id, shard) when is_integer(shard) do
-    millis = :os.system_time(:millisecond) - id.epoch
-    value = millis <<< (id.sequence_bits + id.shard_bits)
-      ||| (id.sequence &&& id.sequence_mask) <<< (id.shard_bits)
-      ||| (shard &&& id.shard_mask)
-
-    id = %{id | sequence: id.sequence + 1}
-    { value, id }
+  def make_shard(text) do
+    hash = :crypto.hash(:sha, text)
+    <<_::binary-18, shard::unsigned-big-integer-16>> = hash
+    shard
   end
 
   @doc """
   Generate an Id Value.
 
   ## Examples
-      iex> id = FlexId.new
-      {value, id} = FlexId.generate(id, "username")
+      iex> {:ok, fid} = FlexId.start_link
+      value = FlexId.generate(fid, 0xB1)
   """
-  def generate(id, text) when is_binary(text) do
-    hash = :crypto.hash(:sha, text)
-    <<_::binary-18, shard::unsigned-big-integer-16>> = hash
-    generate(id, shard)
+  def generate(agent, shard) when is_integer(shard) do
+    Agent.get_and_update(agent, fn state ->
+      millis = :os.system_time(:millisecond) - state.epoch
+      value = millis <<< (state.sequence_bits + state.shard_bits)
+        ||| (state.sequence &&& state.sequence_mask) <<< (state.shard_bits)
+        ||| (shard &&& state.shard_mask)
+
+      { value, %{state | sequence: state.sequence + 1} }
+    end)
   end
+
 end
--- test/flexid_test.exs
+++ test/flexid_test.exs
@@ -1,8 +1,20 @@
 defmodule FlexIdTest do
-  use ExUnit.Case
-  doctest Flexid
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "defaults" do
+    {:ok, fid} = FlexId.start_link()
+    sh = FlexId.make_shard("test")
+    assert 0xBBD3 == sh
+
+    v1 = FlexId.generate(fid, sh)
+
+    now = :os.system_time(:millisecond)
+    assert now - FlexId.extract_millis(fid, v1) < 5
+    assert 0x00 == FlexId.extract_sequence(fid, v1)
+    assert 0xD3 == FlexId.extract_shard(fid, v1)
+
+    v2 = FlexId.generate(fid, sh)
+    assert 0x01 == FlexId.extract_sequence(fid, v2)
   end
+
 end
