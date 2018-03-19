06bfe13ec47afcbe104e20642d80473d97f076ec
Warren Janssens <warren.janssens@gmail.com>
2018-03-19 08:12:07 -0600

    adding checksum

--- lib/flexid.ex
+++ lib/flexid.ex
@@ -8,23 +8,30 @@ defmodule FlexId do
 
   defstruct sequence_bits: 8,
             shard_bits: 8,
+            checksum_bits: 4,
             sequence_mask: 0xFF,
             shard_mask: 0xFF,
-            epoch: 0,
+            checksum_mask: 0x0F,
+            # 2000-01-01 0:00 UTC
+            epoch: 946_684_800_000,
             sequence: 0
 
   def start_link() do
     Agent.start_link(fn -> %FlexId{} end)
   end
 
-  def start_link(epoch, sequence_bits, shard_bits) do
-    Agent.start_link(fn -> %FlexId{
-      epoch: epoch,
-      sequence_bits: sequence_bits,
-      sequence_mask: make_mask(sequence_bits),
-      shard_bits: shard_bits,
-      shard_mask: make_mask(shard_bits)
-    } end)
+  def start_link(epoch, sequence_bits, shard_bits, checksum_bits) do
+    Agent.start_link(fn ->
+      %FlexId{
+        epoch: epoch,
+        sequence_bits: sequence_bits,
+        sequence_mask: make_mask(sequence_bits),
+        shard_bits: shard_bits,
+        shard_mask: make_mask(shard_bits),
+        checksum_bits: checksum_bits,
+        checksum_mask: make_mask(checksum_bits)
+      }
+    end)
   end
 
   def set_sequence(agent, seq) do
@@ -32,36 +39,54 @@ defmodule FlexId do
   end
 
   def extract_raw_millis(agent, value) do
-    Agent.get(agent, fn state -> value >>> (state.sequence_bits + state.shard_bits) end)
+    Agent.get(agent, fn state ->
+      value >>> (state.sequence_bits + state.shard_bits + state.checksum_bits)
+    end)
   end
 
   def extract_millis(agent, value) do
-    Agent.get(agent, fn state -> (value >>> (state.sequence_bits + state.shard_bits)) + state.epoch end)
+    Agent.get(agent, fn state ->
+      (value >>> (state.sequence_bits + state.shard_bits + state.checksum_bits)) + state.epoch
+    end)
   end
 
   def extract_sequence(agent, value) do
-    Agent.get(agent, fn state -> (value >>> state.shard_bits) &&& state.sequence_mask end)
+    Agent.get(agent, fn state ->
+      value >>> (state.shard_bits + state.checksum_bits) &&& state.sequence_mask
+    end)
   end
 
   def extract_shard(agent, value) do
-    Agent.get(agent, fn state -> value &&& state.shard_mask end)
+    Agent.get(agent, fn state -> value >>> state.checksum_bits &&& state.shard_mask end)
+  end
+
+  def extract_checksum(agent, value) do
+    Agent.get(agent, fn state -> value &&& state.checksum_mask end)
   end
 
   def log(agent) do
     Agent.cast(agent, fn state ->
-      ms = round(:math.pow 2, (64 - state.sequence_bits - state.shard_bits - 1))
+      ms =
+        round(:math.pow(2, 64 - state.sequence_bits - state.shard_bits - state.checksum_bits - 1))
+
       yr = round(ms / 1000 / 60 / 60 / 24 / 365)
-      fr = DateTime.from_unix! state.epoch, :millisecond
-      to = DateTime.from_unix! (state.epoch + ms), :millisecond
-      seq = round(:math.pow 2, state.sequence_bits)
-      shd = round(:math.pow 2, state.shard_bits)
-      Logger.info "Ids have a time range of #{yr} years (#{fr} to #{to}), #{seq} sequences, #{shd} shards"
+      fr = DateTime.from_unix!(state.epoch, :millisecond)
+      to = DateTime.from_unix!(state.epoch + ms, :millisecond)
+      seq = round(:math.pow(2, state.sequence_bits))
+      shd = round(:math.pow(2, state.shard_bits))
+
+      Logger.info(
+        "Ids have a time range of #{yr} years (#{fr} to #{to}), #{seq} sequences, #{shd} shards, #{
+          state.checksum_bits
+        } checksum bits"
+      )
+
       state
-   end)
+    end)
   end
 
   defp make_mask(bits) do
-    Enum.reduce(1..bits, 0, fn(_, acc) -> (acc <<< 1) ||| 1 end)
+    Enum.reduce(1..bits, 0, fn _, acc -> acc <<< 1 ||| 1 end)
   end
 
   @doc """
@@ -86,12 +111,41 @@ defmodule FlexId do
   def generate(agent, shard) when is_integer(shard) do
     Agent.get_and_update(agent, fn state ->
       millis = :os.system_time(:millisecond) - state.epoch
-      value = millis <<< (state.sequence_bits + state.shard_bits)
-        ||| (state.sequence &&& state.sequence_mask) <<< (state.shard_bits)
-        ||| (shard &&& state.shard_mask)
 
-      { value, %{state | sequence: state.sequence + 1} }
+      value =
+        millis <<< (state.sequence_bits + state.shard_bits + state.checksum_bits) |||
+          (state.sequence &&& state.sequence_mask) <<< (state.shard_bits + state.checksum_bits) |||
+          (shard &&& state.shard_mask) <<< state.checksum_bits
+
+      value = if state.checksum_bits == 4, do: checksum(value), else: value
+
+      {value, %{state | sequence: state.sequence + 1}}
     end)
   end
 
+  def checksum(input) do
+    {_, sum, _} =
+      1..15
+      |> Enum.reduce({input >>> 4, 0, 2}, fn i, {input, sum, factor} ->
+        addend = factor * (input &&& 0xF)
+        addend = div(addend, 0xF) + rem(addend, 0xF)
+        {input >>> 4, sum + addend, if(factor == 2, do: 1, else: 2)}
+      end)
+
+    remainder = rem(sum, 0xF)
+    check = rem(0xF - remainder, 0xF)
+    input ||| check
+  end
+
+  def verify_checksum(input) do
+    {_, sum, _} =
+      0..15
+      |> Enum.reduce({input, 0, 1}, fn i, {input, sum, factor} ->
+        addend = factor * (input &&& 0xF)
+        addend = div(addend, 0xF) + rem(addend, 0xF)
+        {input >>> 4, sum + addend, if(factor == 2, do: 1, else: 2)}
+      end)
+
+    0 == rem(sum, 0xF)
+  end
 end
--- test/flexid_test.exs
+++ test/flexid_test.exs
@@ -18,21 +18,44 @@ defmodule FlexIdTest do
     assert 0x01 == FlexId.extract_sequence(fid, v2)
   end
 
-  test "sixes" do
-      {:ok, fid} = FlexId.start_link(0, 6, 6)
-      #FlexId.log(fid)
-      sh = FlexId.make_shard("test")
-      assert 0xBBD3 == sh
+  test "eights" do
+    {:ok, fid} = FlexId.start_link(0, 8, 8, 0)
+    FlexId.log(fid)
+    sh = FlexId.make_shard("test")
+    assert 0xBBD3 == sh
 
-      v1 = FlexId.generate(fid, sh)
+    v1 = FlexId.generate(fid, sh)
 
-      now = :os.system_time(:millisecond)
-      assert now - FlexId.extract_millis(fid, v1) < 5
-      assert 0x00 == FlexId.extract_sequence(fid, v1)
-      assert 0x13 == FlexId.extract_shard(fid, v1)
+    now = :os.system_time(:millisecond)
+    assert now - FlexId.extract_millis(fid, v1) < 5
+    assert 0x00 == FlexId.extract_sequence(fid, v1)
+    assert 0xD3 == FlexId.extract_shard(fid, v1)
 
-      v2 = FlexId.generate(fid, sh)
-      assert 0x01 == FlexId.extract_sequence(fid, v2)
+    v2 = FlexId.generate(fid, sh)
+    assert 0x01 == FlexId.extract_sequence(fid, v2)
   end
 
+  test "sixes" do
+    {:ok, fid} = FlexId.start_link(0, 6, 6, 4)
+    FlexId.log(fid)
+    sh = FlexId.make_shard("test")
+    assert 0xBBD3 == sh
+
+    v1 = FlexId.generate(fid, sh)
+
+    now = :os.system_time(:millisecond)
+    assert now - FlexId.extract_millis(fid, v1) < 5
+    assert 0x00 == FlexId.extract_sequence(fid, v1)
+    assert 0x13 == FlexId.extract_shard(fid, v1)
+
+    v2 = FlexId.generate(fid, sh)
+    assert 0x01 == FlexId.extract_sequence(fid, v2)
+  end
+
+  test "checksum" do
+    assert 0 == FlexId.checksum(0x00000000)
+    assert 0x7FFFFFF7 == FlexId.checksum(0x7FFFFFF0)
+    assert !FlexId.verify_checksum(0x7FFFFFF3)
+    assert FlexId.verify_checksum(0x7FFFFFF7)
+  end
 end
